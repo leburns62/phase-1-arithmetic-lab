08638810fe73e91a790a361a8e2e99810757e4aa
Levi Hutchinson Burns <leburns62@gmail.com>
2022-04-27 20:16:30 -0400

    commit tree d4290d887a3dc2dc88ffef515402a8aa4c4cf91c

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31;
+let num2 = 2;
+const multiply = parseInt(num1) * parseInt(num2);
 
-// Write your code here
+const random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 29;
+let num4 = 5;
+const mod = parseInt(num3) % parseInt(num4);
+
+const max = Math.max(17, 18, 19, 20);
